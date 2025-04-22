780abd6b79ca6250339e33990c49487ad702e0a7
metalscratch <140045937+metalscratch@users.noreply.github.com>
2025-04-22 15:21:50 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e3a8a',
+        secondary: '#d1d5db',
+      },
+    },
+  },
+  plugins: [],
+}
